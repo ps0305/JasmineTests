7a441c8fa040363efe54ffe6ba7515c6f2924a2c
ps0305 <pankajsingh0305@gmail.com>
2018-11-16 18:42:58 +0530

    commits

--- index.js
+++ index.js
@@ -2,6 +2,5 @@
 
 import {AppRegistry} from 'react-native';
 import JasmineTests from './App';
-import {name as JasmineTests} from './app.json';
 
 AppRegistry.registerComponent('JasmineTests', () => JasmineTests);
